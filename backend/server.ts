912932beed57a7a6817c37a4a02cb38dc39a1efd
manumay@1962 <manumay8@gmail.com>
2025-11-27 04:48:53 +0530

    cors issue fixed

--- backend/server.ts
+++ backend/server.ts
@@ -12,8 +12,10 @@ const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['https://workflow-1-kq5k.onrender.com/'],
-  credentials: true
+  origin: ['https://workflow-1-kq5k.onrender.com'], 
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 app.use(express.json());
